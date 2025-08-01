adb7e6c9c6ced72c11097b2efcc786531874fb9c
aditikhare1 <aditikhare33@gmail.com>
2025-08-01 18:25:36 -0400

    tighter

--- illustration/misc.js
+++ illustration/misc.js
@@ -1,53 +1,25 @@
 const rootFolder = '/static/digital-media/';
 const items = [
-    { name: 'the michigan daily (2022 - 2023)',
-      images: ['mic1.png', 'mic2.png', 'mic3.png', 'mic4.png'],
-      imageStyling: 'super-short',
-      description: 'Below is a selection of illustrations from my tenure at <a href="https://www.michigandaily.com/michigan-in-color/where-are-you-from-a-reflection-on-residential-segregation"> The Michigan Daily</a>, an independent newspaper serving the broader Ann Arbor, MI community. Specifically, I worked in Michigan in Color section, which is run for and by people of color, collaborating with authors on article graphics.'
-    },
     {
       name: 'animal ABCs (2022)',
       images: ['A-for-Apple-Project.jpg'],
       imageStyling: 'short',
-      description: 'An Animal ABCs poster for a client.'
+      description: 'A vector-based alphabet series designed to engage young learners through friendly, expressive animal characters. All illustrations were built in Adobe Illustrator with clean layers and simple geometry, making them easy to animate or customize.'
+    },
+    { name: 'the michigan daily (2022 - 2023)',
+      images: ['mic1.png', 'mic2.png', 'mic3.png', 'mic4.png'],
+      imageStyling: 'super-short',
+      description: 'Below is a selection of illustrations from my tenure at <a href="https://www.michigandaily.com/michigan-in-color/where-are-you-from-a-reflection-on-residential-segregation"> The Michigan Daily</a>, an independent newspaper serving the broader Ann Arbor, MI community. All works were created in Adobe Suite and procreate'
     },
     { name: "hide and seek; near a blue window (2022)",
       images: ['hide-n-seek.png'], 
       description: `<span class="hidden1"> Digital work, <a href='https://www.ouch.studio/zine/luck'>featured in OUCH! magazine ed.10: Luck</a> </span>`,
       imageStyling: 'tall'
     },
-
-    { name: 'Something of You I Found in Me (2025)',
-      images: ['something-of-me-i-found-in-you.png'],
-      imageStyling: 'tall',
-      description: 'Acrylic and colored pencil on canvas'
-    },
     { name: 'queer in color (2023)',
         images: ['queer-in-color.png'],
         imageStyling: 'sketch-short',
         description: 'Digital graphic for the Michigan in Color, <a href="https://mic.michigandaily.com/queer-in-color-2023/"> Queer in Color</a> website background.'
-    },
-    { name: 'zine covers (2022-2023)',
-      images: ['uaao-zine1.png', 'uaao-zine2.png'],
-      description: `Designed zine covers for United Asian American Organizations, a racial advocacy collective.`,
-      imageStyling: 'short'
-    }, 
-    { name: 'doodles',
-      images: [
-        'sketch/dancing-monster.png',
-        'sketch/yearn.png',
-        'sketch/handy.jpg',
-        'sketch/sunshine.png',
-        'sketch/home.png',
-        'sketch/pinkjob.png',
-        'sketch/egg-sperm.png',
-        'sketch/pinkjob3.png',
-        'sketch/sleepy-shirt.png',
-        'sketch/drool.png',
-        'sketch/basketball.png'
-      ],
-      description: '',
-      imageStyling: 'super-short'
     }
 ]
 
